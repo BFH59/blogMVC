a7ba2b23821325866dbfeaa52a1a7346713ab1d4
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2019-12-27 14:34:24 +0100

    add register form, usercontroller with register function and check if user already exists in db

--- app/Table/UserTable.php
+++ app/Table/UserTable.php
@@ -6,5 +6,12 @@ use \Core\Table\Table;
 
 class UserTable extends Table
 {
+    public function userExists($username, $email){
+        $userExists = $this->db->prepare('SELECT * FROM users WHERE username = ? OR email = ?',[$username, $email], null, true);
+        if($userExists){
+                return true;
+            }
+        return false;
 
+    }
 }
