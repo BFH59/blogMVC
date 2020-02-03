6e798ed17547c33142ec9099fdeb5b5850c4baf7
Julien_Plumecocq <b4ckfr0mh3ll@gmail.com>
2020-02-03 12:26:35 +0100

    create home page controller with description, photo etc - still need to add contact form

---
+++ app/Views/home/index.php
@@ -0,0 +1,30 @@
+<!-- Page Header -->
+<header class="masthead" style="background-image: url('content/startbootstrap-clean-blog-gh-pages/img/home-bg.jpg')">
+    <div class="overlay"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+                <div class="site-heading">
+                    <h2>Julien Plumecocq</h2>
+                    <div class="row">
+                        <div class="col-lg-4 mx-auto">
+                            <img class="rounded-circle img-fluid" src="../public/content/assets/photo.png" alt="photo presentation">
+                        </div>
+                        <div class="col-lg-8 col-md-10 mx-auto">
+                            <br>
+                            <span class="subheading" align="left">Développeur PHP/Symfony/Magento en formation</span>
+                            <p class="subheading" align="left">Passioné par le web depuis toujours, je me forme aux différentes technologies web afin de vous aider à réaliser vos e-projets !</p>
+                        </div>
+                    </div>
+            </div>
+        </div>
+    </div>
+</header>
+
+<div class="container">
+    <div class="row">
+        <div class="col-lg-8 col-md-10 mx-auto">
+            <h2>TEST</h2>
+        </div>
+    </div>
+</div>
--- app/Views/templates/default.php
+++ app/Views/templates/default.php
@@ -34,7 +34,7 @@
 <!-- Navigation -->
 <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
     <div class="container">
-        <a class="navbar-brand" href="index.php">Blog MVC</a>
+        <a class="navbar-brand" href="index.php">Home Page</a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
             Menu
             <i class="fas fa-bars"></i>
@@ -42,13 +42,7 @@
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="index.php">Blog</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="about.html">About</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="contact.html">Contact</a>
+                    <a class="nav-link" href="index.php?p=posts.index">Blog</a>
                 </li>
                 <?php
                 if(isset($_SESSION['usertype']) && $_SESSION['usertype'] === 'admin')
